2640c14ae63835653617e849f3b17a1fbf95fe2b
lyikovgeorgiy <138659008+lyikovgeorgiy@users.noreply.github.com>
2023-07-05 10:47:53 +0300

    Add files via upload

---
+++ Flashing/scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventMenu_Event2_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#b8adea'
+	},
+
+	async EventGameplay_Event1_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#b8adea'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
